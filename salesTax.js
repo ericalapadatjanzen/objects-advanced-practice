8389853ae253dbfba19e4d9f49221e2b72e63436
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-05 22:28:08 +0000

    finished calculating sales and tax

--- salesTax.js
+++ salesTax.js
@@ -1,75 +1,4 @@
-
-
-// function calculateSales(companySalesData, taxRates) {
-//   var output = {};
-//   var totalsales = {};
-//   for (var i = 0; i < companySalesData.length; i++) {
-//     console.log('first loop', companySalesData[i]);
-//     for (k = 0; k < companySalesData[i].sales.length; k++) {
-//       console.log('2nd loop', companySalesData[i].sales[k]);
-//       if (totalsales[companySalesData[i].name]) {
-//         totalsales[companySalesData[i].name] += companySalesData[i].sales[k]; }
-
-//      else {
-//         totalsales[companySalesData[i].name] = companySalesData[i].sales[k];
-//       }
-//     }
-//   }
-// }
-
-// function calculateProvSales(companySalesData, taxRates) {
-//  var provSale = {};
-//  //console.log(int);
-//  for (var i = 0; i < companySalesData.length; i++) {
-//    let company = companySalesData[i];
-
-//   for (var prov in taxRates) {
-//      if (prov === company.province) {
-//        for (var j = 0; j < company.sales.length; j++) {
-//          console.log(taxRates[prov] * company.sales[j]);
-//        }
-//      }
-//    }
-//  }
-// }
-
-function calculateProvSales(companySalesData){
-  var totalSales = {};
-    for(var i = 0; i < companySalesData.length; i++){
-      var company = companySalesData[i];
-
-      if (!totalSales[company.name]){
-          totalSales[company.name] = {
-            totalSales: 0
-          }
-        }
-      // console.log("first loop", company);
-
-      // totalSales[company.name] = {totalSales: 0 }
-      for(var x = 0; x < company.sales.length; x++){
-        totalSales[company.name].totalSales += company.sales[x];
-        // console.log("second loop", company.sales[x]);
-        // var salesLoop = company.sales[x];
-
-        // if (!totalSales[company.name]){
-        //   totalSales[company.name] = {
-        //     totalSales: 0
-        //   }
-        // }
-        //   totalSales[salesLoop].name += company.sales[x];
-        //   }else {
-        //     totalSales[salesLoop].name = company.sales[x];
-        //   }
-
-        // }
-
-
-      // totalSales[company.name].totalSales += company.sales[x];
-    }
-
-    }
-  return totalSales;
-}
+var result = {};
 
 var salesTaxRates = {
   AB: 0.05,
@@ -96,7 +25,41 @@ var companySalesData = [
 
 ];
 
-console.log(calculateProvSales(companySalesData))
+function calculateSales(companySalesData){
+  var totalSales = 0;
+  var totalTax = 0;
+
+  for(var i = 0; i < companySalesData.sales.length; i++){
+        totalSales += companySalesData.sales[i];
+      }
+
+  switch(companySalesData.province){
+    case "AB":
+      totalSalesTax = totalSales * salesTaxRates.AB;
+      break;
+    case "BC":
+      totalSalesTax = totalSales * salesTaxRates.BC;
+      break;
+    case "SK":
+      totalSalesTax = totalSales * salesTaxRates.SK;
+      break;
+  }
+
+ if (result.hasOwnProperty(companySalesData.name)) {
+      result[companySalesData.name].totalSales += totalSales
+      result[companySalesData.name].totalSalesTax += totalSalesTax
+    } else {
+      result[companySalesData.name] = {'totalSales': totalSales, 'totalSalesTax': totalSalesTax}
+    }
+}
+
+
+    for (var i = 0; i < companySalesData.length; i++) {
+  calculateSales(companySalesData[i]);
+};
+
+
+console.log(result);
 
 // console.log(companySalesData)
 
